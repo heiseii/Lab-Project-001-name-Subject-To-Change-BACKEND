42b41c48498a73e325bac4be6579267e9a559473
Lucca <lukaweinzettel@gmail.com>
2025-11-13 20:46:45 -0300

    Add initial users app migration and update settings
    Created the initial migration for the users app, introducing Conversation and Message models. Updated INSTALLED_APPS in settings.py to remove 'users.apps.UsersConfig' and keep 'users'. Database updated accordingly.

--- msj_proyect/settings.py
+++ msj_proyect/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig',
     'users',
     'channels'
 ]
